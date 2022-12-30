96cfd96186e76d68d13eaffa95bfabbab593f7f2
Dmitriy Svetilov <>
2022-12-30 16:35:09 +0300

    scene and fix prefabs

--- Assets/Project/Scripts/BuildPoint/Multiplier.cs
+++ Assets/Project/Scripts/BuildPoint/Multiplier.cs
@@ -1,7 +1,15 @@
+using UnityEngine;
+
 namespace Project
 {
     public class Multiplier : BuildingBase
     {
-       
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.TryGetComponent(out Ball ball))
+            {
+                
+            }
+        }
     }
 }
